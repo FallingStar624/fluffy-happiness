fea28f443ec5e3c0932ec2e7ece9e3bac5e51482
ks94love <ks94love@gmail.com>
2021-11-17 19:41:40 +0900

    update models.py

---
+++ movies/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+class Movie(models.Model):
+    movie_id = models.IntegerField()
+    title = models.CharField(max_length=100)
+    release_date = models.DateField()
+    popularity = models.FloatField()
+    vote_count = models.IntegerField()
+    vote_average = models.FloatField()
+    overview = models.TextField()
+    poster_path = models.CharField(max_length=200, null=True)
+    genres = models.ManyToManyField(Genre)
+    recommend = models.ManyToManyField('self', symmetrical=False, related_name='recommeded')
+    watch_users = models.ManyToManyField(settings.AUTH_USER_MODEL, symmetrical=False, related_name='watch_movies', through='CheckTime')
+
+
+
+class MovieComment(models.Model):
+    content = models.TextField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_comments')
+
+
+class CheckTime(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    check_time = models.DateTimeField(auto_now_add=True)
+
+
