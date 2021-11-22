b02d26d50255bd5f11112fbc83accd4bb7d20e63
ks94love <ks94love@gmail.com>
2021-11-22 19:01:00 +0900

    update css

--- fluffyhappiness/settings.py
+++ fluffyhappiness/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'movies',
     'bootstrap5',
     'imagekit',
+    'mathfilters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
